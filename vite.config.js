8d6fe2ba5a9b7fa3da08df7ca73770f807d35e80
Pratik Bakshi <bakshi.pratikcool@gmail.com>
2026-01-02 15:59:17 +0530

    deploy build corrected

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/image_slider/",
+  build: {
+    outDir: "docs", // ⬅️ output folder
+    emptyOutDir: true, // clears old files before build
+  },
 });
